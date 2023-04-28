a4a65f4da4114f7ed8a597447a9a21af78409034
theanh <theanhvo221@gmail.com>
2023-04-28 09:26:09 +0700

    support chain ETHER

--- enum.py
+++ enum.py
@@ -9,7 +9,8 @@ class Chains:
     BSC = 'BSC'
     AVAX = 'AVAX'
     POLYGON = 'POLYGON'
-    BOBA = 'BOBA'
+    BOBA = 'BOBA',
+    ETHER = 'ETHER'
 
 
-ListChainsSupport = [Chains.BSC, Chains.AVAX, Chains.BOBA]
+ListChainsSupport = [Chains.BSC, Chains.AVAX, Chains.BOBA, Chains.ETHER]
